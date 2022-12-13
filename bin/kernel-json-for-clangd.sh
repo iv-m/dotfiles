3b6d37ec2bf044874c8bdaf5a166978cd57c36b8
Ivan A. Melnikov <iv@altlinux.org>
2022-12-13 10:45:36 +0400

    bin: Add kernel-json-for-clangd.sh
    A simple wrapper for linux kernel compile_commands.json
    generator that adds various cleanups, so that clangd
    does not warn about unknown options.

---
+++ bin/kernel-json-for-clangd.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -xue
+
+[ -d .git ]
+[ -f .config ]
+
+./scripts/clang-tools/gen_compile_commands.py "$@"
+
+# some cleanups
+# - remove options that are not supported by clang
+#   and should not affect parsing
+# - rewrite target into something clang can digest
+sed -i \
+    -e 's/-fno-allow-store-data-races//g'   \
+    -e 's/-mstack-protector-guard\S\+//g'    \
+    -e 's/-fconserve-stack//g'              \
+    -e 's/-ftrivial-auto-var-init=\S\+//g'   \
+    -e 's/rv64ima\S\+/rv64gc/g'             \
+    compile_commands.json
