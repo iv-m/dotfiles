77ee7e455e7f77f2d3d93e30d7fbbbc1e519de78
Ivan A. Melnikov <iv@altlinux.org>
2022-12-13 10:40:33 +0400

    nvim-cmp: Adjusted completion hotkeys

--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -180,11 +180,18 @@ cmp.setup {
   mapping = cmp.mapping.preset.insert {
     ['<C-d>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete(),
-    ['<CR>'] = cmp.mapping.confirm {
-      behavior = cmp.ConfirmBehavior.Replace,
-      select = true,
-    },
+    ['<C-Space>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        return cmp.complete_common_string()
+      end
+      return cmp.complete()
+    end),
+    ['<CR>'] = cmp.mapping(function(fallback)
+      if cmp.get_selected_entry() then
+        return cmp.confirm()
+      end
+      return fallback()
+    end),
     ['<C-k>'] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
