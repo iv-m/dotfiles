287dde6dbee9808f784a4487169269fe388cf62a
Ivan A. Melnikov <iv@altlinux.org>
2023-03-09 23:51:02 +0400

    nvim: cmp: Avoid using presets
    ... and also avoid touching insert-mode ^P. I love
    my insert-mode ^P. Keyword completions are awesome.

--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -263,7 +263,9 @@ cmp.setup {
       luasnip.lsp_expand(args.body)
     end,
   },
-  mapping = cmp.mapping.preset.insert {
+  mapping = {
+    ['<Down>'] = cmp.mapping.select_next_item(),
+    ['<Up>'] = cmp.mapping.select_prev_item(),
     ['<C-d>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping(function(fallback)
